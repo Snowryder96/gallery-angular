d95b0d6851c34a7dbf74ef99a330073bb2444189
Alex <47950293+Snowryder96@users.noreply.github.com>
2020-11-02 16:45:11 +0100

    Added custom light/dark theme

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavigationComponent implements OnInit {
+  @Input()
+  isDarkTheme : boolean = true;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+  }
+
+  storeThemeSelection(){
+    localStorage.setItem('theme', this.isDarkTheme? "Dark" : "Light");
+  }
+
+}
